7d12db82101a3760988516570fc27038c1639b87
thr3a <12jk172@ms.dendai.ac.jp>
2017-07-01 20:08:22 +0900

    pumaが本番環境で動くように

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.7.2"
 
 set :application, "mecab_api"
-set :repo_url, "git@github.com:thr3a/mecab_api_sinatra.git"
+set :repo_url, "https://github.com/thr3a/mecab_api_sinatra.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -24,13 +24,21 @@ set :repo_url, "git@github.com:thr3a/mecab_api_sinatra.git"
 # append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
+
+set :puma_threads,    [4, 16]
+set :puma_workers,    1
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{shared_path}/log/puma.error.log"
+set :puma_error_log,  "#{shared_path}/log/puma.access.log"
 
 namespace :deploy do
 
